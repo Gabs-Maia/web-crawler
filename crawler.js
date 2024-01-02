dd705010d53f9ef0e892bb7c8af0168211859e49
Gabs-Maia <gabrielsouzamaia99@gmail.com>
2024-01-02 12:23:45 -0300

    Add functions to retireve hrefs from a given url and more tests.

--- crawler.js
+++ crawler.js
@@ -1,4 +1,60 @@
 //Normalize urls when needed...
+const cheerio = require('cheerio');
+const axios = require('axios');
+const {JSDOM} = require('jsdom');
+
+function get_urls_from(html_body, input_url){
+
+    const urls = [];
+    const dom = new JSDOM(html_body);
+    const links = dom.window.document.querySelectorAll('a');
+
+    for(const link of links){
+
+        if(link.href.slice(0, 1) === '/'){
+
+            try{
+                const url_obj = new URL(`${input_url}${link.href}`);
+                urls.push(url_obj.href);
+            }catch(err){
+                console.log(`error with relative url :- ${err.message}`);
+            }
+        }else{
+            try{
+
+                const url_obj = new URL(link.href);
+                urls.push(url_obj.href);
+            }catch (err){
+                console.log(`error with absolute url :- ${err.message}`);
+            }
+        }
+    }
+    return urls;
+}
+
+async function get_urls(url){
+    try{
+    let urls = [];
+    let{data} = axios.get(url);
+    let load = cheerio.load(data);
+    let a_tags = load("body");
+
+    for(let i = 0; i < a_tags.length; i ++){
+
+        let href = load(a_tags).find("a").attr("href");
+
+        if(href !== null){
+            urls.push(href);
+        }else{
+            return urls;
+        }
+    }
+    return urls;    
+}catch(error){
+    console.error(`Could not conclude the operations due to :- ${error}`);
+}
+}
+
 function normalURL(url_string){
     
     const objURL = new URL(url_string);
@@ -11,5 +67,6 @@ function normalURL(url_string){
 }
 module.exports = {
 
-    normalURL
+    normalURL,
+    get_urls_from
 }
--- crawler.test.js
+++ crawler.test.js
@@ -1,4 +1,4 @@
-const {normalURL} = require('./crawler.js')
+const {normalURL, get_urls_from} = require('./crawler.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalURL strip', () => {
@@ -28,4 +28,87 @@ test('normalURL strip http', () => {
     const output_url = normalURL(input_url);
     const expected_url = 'blog.maia.dev/path';
     expect(output_url).toEqual(expected_url);
+})
+
+test('get_urls_from', () =>{
+    const html_body = `
+    <html>
+        <body>
+            <a href="https://blog.maia.dev/">
+                Maia.dev blog
+            </a>
+        </body>
+    </html>
+    `
+    const input_url = "https://blog.maia.dev"
+    const outputed = get_urls_from(html_body, input_url);
+    const expected = ["https://blog.maia.dev/"];
+    expect(outputed).toEqual(expected);
+})
+
+test('get_urls_from absolute', () =>{
+    const html_body = `
+    <html>
+        <body>
+            <a href="https://blog.maia.dev/path/">
+                Maia.dev blog
+            </a>
+        </body>
+    </html>
+    `
+    const input_url = "https://blog.maia.dev/path/"
+    const outputed = get_urls_from(html_body, input_url);
+    const expected = ["https://blog.maia.dev/path/"];
+    expect(outputed).toEqual(expected);
+})
+
+test('get_urls_from relative', () =>{
+    const html_body = `
+    <html>
+        <body>
+            <a href="/path/">
+                Maia.dev blog
+            </a>
+        </body>
+    </html>
+    `
+    const input_url = "https://blog.maia.dev"
+    const outputed = get_urls_from(html_body, input_url);
+    const expected = ["https://blog.maia.dev/path/"];
+    expect(outputed).toEqual(expected);
+})
+
+test('get_urls_from both', () =>{
+    const html_body = `
+    <html>
+        <body>
+            <a href="https://blog.maia.dev/path1/">
+                Maia.dev blog path one
+            </a>
+            <a href="/path2/">
+                Maia.dev blog path two
+            </a>
+        </body>
+    </html>
+    `
+    const input_url = "https://blog.maia.dev"
+    const outputed = get_urls_from(html_body, input_url);
+    const expected = ["https://blog.maia.dev/path1/", "https://blog.maia.dev/path2/"];
+    expect(outputed).toEqual(expected);
+})
+
+test('get_urls_from invalid', () =>{
+    const html_body = `
+    <html>
+        <body>
+            <a href="invalid">
+                Invalid URL
+            </a>
+        </body>
+    </html>
+    `
+    const input_url = "https://blog.maia.dev"
+    const outputed = get_urls_from(html_body, input_url);
+    const expected = [];
+    expect(outputed).toEqual(expected);
 })
